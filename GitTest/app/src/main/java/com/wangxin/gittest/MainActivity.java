0c9b7e306b38f44f811623dcc74fb93617c1d5cd
wangxin <wangxin@MacBook-Pro.local>
2016-06-07 10:31:58 +0800

    branch1 commit init TextView

--- GitTest/app/src/main/java/com/wangxin/gittest/MainActivity.java
+++ GitTest/app/src/main/java/com/wangxin/gittest/MainActivity.java
@@ -2,12 +2,17 @@ package com.wangxin.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.tv);
     }
 }
